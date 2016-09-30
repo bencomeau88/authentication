4b31fd0adc801afe4170ec5137ab6a3aa2d7aef6
Benjamin Comeau <bcomeau@gwmail.gwu.edu>
2016-09-30 12:20:23 -0400

    '/hidden' section isn't working

---
+++ index.js
@@ -0,0 +1,152 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var User = require('./user-model');
+var bcrypt = require('bcryptjs');
+var app = express();
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+var jsonParser = bodyParser.json();
+
+
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
+    });
+});
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+
+
+app.get('/hidden', passport.authenticate('basic', {session: false}), function(req, res) {
+   return res.json({
+        message: 'Luke... I am your father'
+    });
+});
+
+// app.get('/users', function(req, res){
+//     User.find({},function(err,users){
+//         if (err) res.status(500).json({message:'internal server error'});
+//         return res.status(200).json(users);
+//     });
+// });
+
+app.post('/users', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.status(400).json({
+            message: "No request body"
+        });
+    }
+
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
+        });
+    }
+
+    var username = req.body.username;
+
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+
+    username = username.trim();
+
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+
+    var password = req.body.password;
+
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+
+   bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error'
+                    });
+                }
+
+                return res.status(201).json({});
+            });
+        });
+    });
+
+
+});
+
+mongoose.connect('mongodb://localhost/auth').then(function() {
+    User.find(function(err,users){
+        console.log(users);
+    });
+    app.listen(process.env.PORT || 8080);
+});
+
